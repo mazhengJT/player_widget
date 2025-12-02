eb3097ba4cbc60f0da3110b638b7889d9f354d50
mz <>
2025-12-02 09:07:52 +0800

    增加左绘右彩播放设置

--- lib/aliplayer_widget_controller.dart
+++ lib/aliplayer_widget_controller.dart
@@ -881,6 +881,18 @@ class AliPlayerWidgetController {
     mirrorModeNotifier.value = newValue;
   }
 
+  /// 设置
+  ///
+  /// Set the alphaRender mode of the player.
+  ///
+  /// [AlphaRenderMode] The target alphaRender mode.
+  Future<void> setAlphaRenderMode(int alphaRenderModel) async {
+    await _aliPlayer.setAlphaRenderMode(alphaRenderModel);
+    int newValue = await _aliPlayer.getAlphaRenderMode();
+
+    logi("setMirrorMode: $alphaRenderModel, real: $newValue");
+  }
+
   /// 设置旋转角度
   ///
   /// Set the rotation angle of the player.
